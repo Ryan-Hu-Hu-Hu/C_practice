c7141637a371180d9433e816d7013c819583c7b2
Ryan-Hu-Hu-Hu <ryanhu089@gmail.com>
2024-07-30 22:07:08 +0800

    Add  printf function call

--- Ch2/pun.c
+++ Ch2/pun.c
@@ -2,5 +2,7 @@
 int main(void)
 {
     printf("To C, or not to C, this is the question \n");
+    printf("Brevity is the soul of wit. \n  --Shakespeare \n");
     return 0;
+    //or use exit(0) is also acceptable 
 }
